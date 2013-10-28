911f1743b79daddfc5d77f565ed4be31e997086d
Ruslan <roastedbard@gmail.com>
2013-10-28 15:48:43 +0200

    Added methods for handling colliders collisions

--- Core/Physics.cpp
+++ Core/Physics.cpp
@@ -1,4 +1,5 @@
 #include "Physics.h"
+#include <cmath>
 #include <iostream>
 
 bool Physics::isIntersects(GameObject *a, GameObject *b, Direction::Id id)
@@ -21,3 +22,34 @@ bool Physics::isIntersects(GameObject *a, GameObject *b, Direction::Id id)
 	return true;
 }
 
+bool Physics::isSphereAndSphereCollides(SphereCollider colliderA, SphereCollider colliderB)
+{
+	double xDistance = std::pow((colliderB.getCenter().x - colliderA.getCenter().x), 2);
+	double yDistance = std::pow((colliderB.getCenter().y - colliderA.getCenter().y), 2);
+
+	double distance = std::sqrt(xDistance + yDistance);
+
+	int radiusSum = colliderA.getRadius() + colliderB.getRadius();
+
+	if(distance <= radiusSum)
+		return true;
+
+	return false;
+}
+
+bool Physics::isBoxAndBoxCollides(BoxCollider colliderA, BoxCollider colliderB)
+{
+	float Tx = abs( ((colliderA.getTop().x + colliderA.getBottom().x) / 2) - ((colliderB.getTop().x + colliderB.getBottom().x) / 2) );
+	float Ty = abs( ((colliderA.getTop().y + colliderA.getBottom().y) / 2) - ((colliderB.getTop().y + colliderB.getBottom().y) / 2) );
+
+	float Ax = abs((colliderA.getBottom().x - colliderA.getTop().x) / 2);
+	float Ay = abs((colliderA.getTop().y    - colliderA.getBottom().y) / 2);
+
+	float Bx = abs((colliderB.getBottom().x - colliderB.getTop().x) / 2);
+	float By = abs((colliderB.getTop().y    - colliderB.getBottom().y) / 2);
+
+	if(Tx >= Ax + Bx || Ty >= Ay + By)
+		return false;
+
+	return true;
+}
--- Core/Physics.h
+++ Core/Physics.h
@@ -2,11 +2,19 @@
 #define	PACMAN_PHYSICS_H
 
 #include "GameObject.h"
+#include "Collider.h"
+#include "BoxCollider.h"
+#include "SphereCollider.h"
 
 class Physics
 {
 public:
 	static bool isIntersects(GameObject *a, GameObject *b, Direction::Id id);
+
+private:
+	static bool isSphereAndSphereCollides(SphereCollider colliderA, SphereCollider colliderB);
+	static bool isBoxAndBoxCollides(BoxCollider colliderA, BoxCollider colliderB);
+	static bool isSphereAndBoxCollides(SphereCollider colliderA, BoxCollider colliderB){};
 };
 
 #endif
